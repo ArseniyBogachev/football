7fb2cdde77edbbb17ea54bf599be271191b85890
Arseniy Bogachev <senya.boagchev@mail.ru>
2022-06-20 01:40:00 +0300

    Create Navbar and Profile (Mobile, comp)

---
+++ frontend/football/src/components/NavbarCom.vue
@@ -0,0 +1,102 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-default bg-dark" style="box-shadow: 0 0 5px 1px darkgray">
+    <div class="container-lg">
+        <div class="navbar-header">
+          <a href="#" class="navbar-brand" @click="$router.push('/')" style="color: gray">Football analytics</a>
+        </div>
+
+        <div>
+          <ul class="nav navbar-nav">
+            <li><a href="#" @click="$router.push('/')" class="nav-link" style="color: gray">Home</a></li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: gray">
+                Teams
+              </a>
+              <ul class="dropdown-menu bg-light" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="#">Premier League</a></li>
+                <li><a class="dropdown-item" href="#">EFL Championship</a></li>
+                <li><a class="dropdown-item" href="#">EFL League One</a></li>
+                <li><a class="dropdown-item" href="#">EFL League Two</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">National League</a></li>
+                <li><a class="dropdown-item" href="#">National League North</a></li>
+                <li><a class="dropdown-item" href="#">National League South</a></li>
+              </ul>
+            </li>
+            <li><a href="#" class="nav-link"  style="color: gray">Article</a></li>
+            <li><a href="#" @click="$router.push('/about')" class="nav-link" style="color: gray">About</a></li>
+            <li><a href="#" class="nav-link" style="color: gray">Link</a></li>
+          </ul>
+        </div>
+
+        <div v-if="false">
+          <ul class="nav navbar-nav">
+            <li><a href="#" class="nav-link"  style="color: gray">Login</a></li>
+            <li><a href="#" class="nav-link" style="color: gray">Register</a></li>
+          </ul>
+        </div>
+        <div v-else>
+          <ul class="nav navbar-nav">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown border-gray" href="#" role="button" id="user" data-bs-toggle="dropdown" aria-expanded="false">
+                <fa class="icon-user" v-if="true" icon="user"></fa>
+                <img v-else class="image-user" src="../assets/user.jpg"/>
+                <span class="name">Arseniy</span>
+              </a>
+              <ul class="dropdown-menu bg-light" aria-labelledby="user">
+                <li><a class="dropdown-item" href="#" @click="$router.push('/profile')">Profile</a></li>
+                <li><a class="dropdown-item" href="#">Settings</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">Logout</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavbarCom"
+}
+</script>
+
+<style scoped>
+  .dropdown-item:active{
+    background-color: gray;
+  }
+  .image-user{
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    border: 1px solid gray;
+  }
+  .name{
+    color: gray;
+    margin-left: 10px;
+    font-size: 12px;
+    font-family: "Arial Unicode MS";
+    letter-spacing: 3px;
+  }
+  .icon-user{
+    color: gray;
+  }
+  .border-gray{
+    border: 1px solid gray;
+    border-radius: 6px;
+    width: 150px;
+    height: 45px;
+    text-align: center;
+  }
+  .border-gray:focus{
+    box-shadow: 0 0 5px 1px black inset;
+  }
+  .image-user-m{
+    width: 17px;
+    height: 17px;
+    border-radius: 50%;
+    border: 1px solid gray;
+
+  }
+</style>
---
+++ frontend/football/src/components/ProfileMob.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="box">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
+      <div class="container-fluid">
+        <img class="image-user" src="../assets/user.jpg" alt="">
+        <a class="navbar-brand" href="#"></a>
+        <ul class="nav justify-content-center">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">My subscriptions</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">My article</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Bookmarks</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <div class="nav-small">
+      <div class="name">kuwsh1n</div>
+      <div class="rate">
+        <div class="following"><fa icon="fa-solid fa-users"></fa><span style="margin-left: 5%">28k</span></div>
+        <div class="likes" style="margin-left: 15%"><fa icon="fa-solid fa-heart"></fa><span style="margin-left: 5%">100k</span></div>
+      </div>
+    </div>
+    <div class="content">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProfileMob"
+}
+</script>
+
+<style scoped>
+  .box{
+    width: 100%;
+    height: 70em;
+    margin-top: 102px;
+  }
+  .image-user{
+    max-width: 50px;
+    max-height: 50px;
+    position: absolute;
+    border-radius: 50%;
+    top: -30px;
+    left: 20px;
+    box-shadow: 0 0 5px 1px black;
+  }
+  .nav-link{
+    color: rgb(40, 40, 40);
+    font-size: 12px;
+  }
+  .nav-small{
+    width: 100%;
+    height: 30px;
+    background-color: rgb(150, 150, 150);
+    display: flex;
+    justify-content: space-between;
+    padding: 3px 40px 3px 20px;
+  }
+  .following{
+    font-size: 12px;
+    display: flex;
+    justify-content: space-between;
+  }
+  .likes{
+    font-size: 12px;
+    display: flex;
+    justify-content: space-between;
+  }
+  .rate{
+    margin-top: 5px;
+    display: flex;
+    justify-content: space-between;
+  }
+  .name{
+    font-family: Georgia;
+    color: rgb(50, 50, 50);
+  }
+  .content{
+    height: 100%;
+    background-color: rgb(220,220,220);
+  }
+</style>

472b26bfa46e5deef9704f0e266412d56f687d61
Arseniy Bogachev <senya.boagchev@mail.ru>
2022-08-04 01:10:21 +0300

    create registeraxios (vue) and jwt token

--- backend/football/startapp/urls.py
+++ backend/football/startapp/urls.py
@@ -1,8 +1,15 @@
-from django.urls import path
+from django.urls import path, re_path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
 from .views import *
 
 urlpatterns = [
     path('api/v1/articles', ArticlesAPIList.as_view(), name='article_list'),
     path('api/v1/articlesrelation', ArticlesRelationAPIList.as_view(), name='relation_list'),
     path('api/v1/articlescategory', ArticlesCategoryAPIList.as_view(), name='category_list'),
+    re_path(r'^auth/', include('djoser.urls')),
+    re_path(r'^auth/', include('djoser.urls.jwt')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
--- frontend/football/src/pages/RegisterApp.vue
+++ frontend/football/src/pages/RegisterApp.vue
@@ -3,45 +3,78 @@
     <form class="container">
       <div class="name">
         <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">Password</label>
-          <input type="password" class="form-control" id="exampleInputFirstName" style="width: 195px;">
+          <label for="exampleInputPassword1" class="form-label">Firstname</label>
+          <input type="text" class="form-control" id="exampleInputFirstName" style="width: 195px;" v-model="firstname">
         </div>
         <div class="mb-3" style="margin-left: 10px">
-          <label for="exampleInputPassword1" class="form-label">Password</label>
-          <input type="password" class="form-control" id="exampleInputLastName" style="width: 195px;">
+          <label for="exampleInputPassword1" class="form-label">Lastname</label>
+          <input type="text" class="form-control" id="exampleInputLastName" style="width: 195px;" v-model="lastname">
         </div>
       </div>
-
       <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" v-model="email">
         <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
       </div>
       <div class="mb-3">
         <label for="exampleInputPassword1" class="form-label">Nick name</label>
-        <input type="password" class="form-control" id="exampleInputNickName">
+        <input type="text" class="form-control" id="exampleInputNickName" v-model="nickname">
       </div>
       <div class="mb-3">
         <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
+        <input type="password" class="form-control" id="exampleInputPassword1" v-model="password">
       </div>
       <div class="mb-3">
         <label for="exampleInputPassword1" class="form-label">Reload Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword2">
+        <input type="password" class="form-control" id="exampleInputPassword2" v-model="password2">
+        <small v-if="this.check_password" style="color: red" ref="check_password">Passwords do not match</small>
       </div>
       <div class="mb-3 form-check">
         <input type="checkbox" class="form-check-input" id="exampleCheck1">
         <label class="form-check-label" for="exampleCheck1">Check me out</label>
       </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
+      <button type="button" class="btn btn-dark" @click="register_axios">Submit</button>
     </form>
   </div>
-
 </template>
 
 <script>
+import axios from "axios";
 export default {
-  name: "RegisterApp"
+  name: "RegisterApp",
+  data(){
+    return{
+      firstname: '',
+      lastname: '',
+      email: '',
+      nickname: '',
+      password: '',
+      password2: '',
+      check_password: false,
+    }
+  },
+  methods:{
+    async register_axios(){
+      try {
+        if (this.password === this.password2){
+          const response = await axios.post('http://127.0.0.1:8000/auth/users/', {
+            'first_name': this.firstname,
+            'last_name': this.lastname,
+            'email': this.email,
+            'username': this.nickname,
+            'password': this.password,
+          })
+          location.reload()
+        }
+        else{
+          this.check_password = true
+        }
+      }
+      catch (e) {
+        console.log(e)
+      }
+    }
+  }
 }
 </script>
 

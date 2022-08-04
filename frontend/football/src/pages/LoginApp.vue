93e749087e6df7ecd4e0d3426e1c8236fc2ba4dd
Arseniy Bogachev <senya.boagchev@mail.ru>
2022-08-05 00:13:00 +0300

    create login app

--- frontend/football/src/pages/LoginApp.vue
+++ frontend/football/src/pages/LoginApp.vue
@@ -1,28 +1,57 @@
 <template>
   <div class="container-md">
     <form class="container">
+      <small v-if="this.axios_error" style="color: red" ref="check_password">{{ this.axios_error }}</small>
       <div class="mb-3">
-        <label for="exampleInputEmail1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <label for="exampleInputEmail1" class="form-label">Nickname</label>
+        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="usernameHelp" v-model="username">
         <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
       </div>
       <div class="mb-3">
         <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
+        <input type="password" class="form-control" id="exampleInputPassword1" v-model="password">
       </div>
       <div class="mb-3 form-check">
         <input type="checkbox" class="form-check-input" id="exampleCheck1">
         <label class="form-check-label" for="exampleCheck1">Check me out</label>
       </div>
-      <button type="submit" class="btn btn-dark">Submit</button>
+      <button type="button" class="btn btn-dark" @click="login_axios">Submit</button>
     </form>
   </div>
 
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: "LoginApp"
+  name: "LoginApp",
+  data(){
+    return{
+      username: '',
+      password: '',
+      axios_error: '',
+    }
+  },
+  methods:{
+    async login_axios(){
+      try {
+        const response = await axios.post('http://127.0.0.1:8000/api/token/', {
+          'username': this.username,
+          'password': this.password,
+        })
+        this.$localStorage.set('access', response.data.access)
+        this.$localStorage.set('refresh', response.data.refresh)
+        console.log(this.$localStorage.get('access'))
+        console.log(this.$localStorage.get('refresh'))
+        location.reload()
+      }
+      catch (e) {
+        console.log(e)
+        this.axios_error = e.response.data.detail
+      }
+    }
+  }
 }
 </script>
 
@@ -31,7 +60,7 @@ export default {
     height: 758px;
   }
   .form-control{
-    width: 400px;
+    max-width: 400px;
   }
   input:focus{
     box-shadow: 0 0 5px 1px #a4a0a2 inset;
@@ -39,9 +68,9 @@ export default {
   }
   .container{
     box-shadow: 0 0 5px 1px gray;
-    width: 500px;
+    max-width: 500px;
     padding: 4%;
-    height: 500px;
+    min-height: 500px;
   }
   .mb-3{
     margin-top: 30px;
--- frontend/football/src/pages/RegisterApp.vue
+++ frontend/football/src/pages/RegisterApp.vue
@@ -20,7 +20,7 @@
 <!--        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>-->
       </div>
       <div class="mb-3">
-        <label for="exampleInputPassword1" class="form-label">Nick name</label>
+        <label for="exampleInputPassword1" class="form-label">Nickname</label>
         <input type="text" class="form-control" id="exampleInputNickName" v-model="username">
         <small v-if="this.axios_error.username" style="color: red" ref="check_password">{{ this.axios_error.username[0] }}</small>
       </div>

3237743efb24ec3b52311eaefac37a71ab7eea3a
Arseniy Bogachev <senya.boagchev@mail.ru>
2023-01-26 03:19:15 +0300

    smtp env

--- backend/football/football/settings.py
+++ backend/football/football/settings.py
@@ -152,8 +152,8 @@ MEDIA_URL = '/media/'
 
 # smtp
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST_USER = 'footballapplicationauth@gmail.com'
-EMAIL_HOST_PASSWORD = 'apfgxjtkufxamswl'
+EMAIL_HOST_USER = os.getenv('SMTP_USER')
+EMAIL_HOST_PASSWORD = os.getenv('SMTP_PASSWORD')
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
